b2ea688470416ca3f37a6c69e88f756033f95986
Tonin <117942487+Toninga@users.noreply.github.com>
2026-02-05 10:44:43 +0100

    Shooter update, added ServiceLocator (Reference.cs)

---
+++ Assets/1. Scripts/Utility/IManager.cs
@@ -0,0 +1,12 @@
+namespace GameSystem
+{
+    public interface IManager
+    {
+        // Register should always be the only method called in a manager's constructor
+        public void Register();
+        // Reference.SetManager(this);
+        public void Unregister();
+        // Reference.RemoveManager(this);
+
+    }
+}
