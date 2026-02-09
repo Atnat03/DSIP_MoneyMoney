72777f838305a8441649e945e3422fe44fececea
Entity02 <114674519+Entity02@users.noreply.github.com>
2026-02-09 19:13:27 +0100

    Map clean

--- Assets/1. Scripts/Bandits/Spawn/SpawnBarrage.cs
+++ Assets/1. Scripts/Bandits/Spawn/SpawnBarrage.cs
@@ -11,9 +11,12 @@ public class SpawnBarrage : MonoBehaviour
         public Transform end;
     }
 
-    public Voiture[] voitures;
+    public Voiture[] voituresSouthWest, voituresNorthEast;
 
     public GameObject prefabBandit;
+    public bool isSouthEast;
+
+    public bool vertical;
 
     void OnTriggerEnter(Collider other)
     {
@@ -21,14 +24,37 @@ public class SpawnBarrage : MonoBehaviour
         {
             BanditSpawnManager.instance.hasToSpawnBarrage = false;
             BanditSpawnManager.instance._timeUntilBanditBarrage = BanditSpawnManager.instance.timeUntilBanditBarrage;
-            
-            foreach (Voiture voiture in voitures)
+
+            if (vertical)
             {
-                GameObject bandit = Instantiate(prefabBandit, voiture.start.position, voiture.start.rotation);
-                bandit.GetComponent<BanditBarrage>().pointA = voiture.start;
-                bandit.GetComponent<BanditBarrage>().pointB = voiture.end;
-                bandit.GetComponent<NetworkObject>().Spawn();
+                isSouthEast = other.transform.position.x > transform.position.x;
             }
+            else
+            {
+                isSouthEast = other.transform.position.z < transform.position.z;
+            }
+            
+            if (isSouthEast)
+            {
+                foreach (Voiture voiture in voituresSouthWest)
+                {
+                    GameObject bandit = Instantiate(prefabBandit, voiture.start.position, voiture.start.rotation);
+                    bandit.GetComponent<BanditBarrage>().pointA = voiture.start;
+                    bandit.GetComponent<BanditBarrage>().pointB = voiture.end;
+                    bandit.GetComponent<NetworkObject>().Spawn();
+                } 
+            }
+            else
+            {
+                foreach (Voiture voiture in voituresNorthEast)
+                {
+                    GameObject bandit = Instantiate(prefabBandit, voiture.start.position, voiture.start.rotation);
+                    bandit.GetComponent<BanditBarrage>().pointA = voiture.start;
+                    bandit.GetComponent<BanditBarrage>().pointB = voiture.end;
+                    bandit.GetComponent<NetworkObject>().Spawn();
+                } 
+            }
+            
         }
     }
     
