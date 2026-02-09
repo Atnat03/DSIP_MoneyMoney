87ffc7bff92e95397c82b6fe631b3d5213b4a38d
BOURRICAUD Antoine <a.bourricaud@rubika-edu.com>
2026-02-09 14:39:04 +0100

    fix

--- Assets/1. Scripts/Interact/Sangles.cs
+++ Assets/1. Scripts/Interact/Sangles.cs
@@ -1,3 +1,5 @@
+using Unity.Netcode;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Sangles : MonoBehaviour, IInteractible
@@ -9,6 +11,10 @@ public class Sangles : MonoBehaviour, IInteractible
         get { return interactionName; }
         set { interactionName = value; }
     }
+    
+    [SerializeField]private GameObject interactionObject = null;
+    private Rigidbody rb;
+    private Collider collider;
 
     public string interactionName;
     
@@ -25,26 +31,61 @@ public class Sangles : MonoBehaviour, IInteractible
     private void HitInteract(GameObject obj, GameObject player)
     {
         if (obj.GetInstanceID() != gameObject.GetInstanceID()) return;
-        
-        
 
         Debug.Log("Hit interact");
 
+        if (interactionObject == null)
+        {
+            StockObjct(player);
+        }
+        else
+        {
+            ReleaseObject(player, true);
+        }
+    }
+
+    private void ReleaseObject(GameObject player, bool isGrabbing)
+    {
+        interactionObject.gameObject.layer = LayerMask.NameToLayer("Interactable");
+        
+        GrabPoint grabPoint = player.GetComponent<GrabPoint>();
+        if (grabPoint != null)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            rb = null;
+            
+            collider.enabled = true;
+            
+            if(isGrabbing)
+                grabPoint.TryGrab(interactionObject.GetComponent<NetworkObject>());
+        }
+
+        interactionObject = null;
+    }
+
+    private void StockObjct(GameObject player)
+    {
         GameObject handObject = null;
         
         GrabPoint grabPoint = player.GetComponent<GrabPoint>();
         if (grabPoint != null)
         {
             handObject = grabPoint.GetCurrentObjectInHand();
-            
             grabPoint.Throw();
+            rb = handObject.GetComponent<Rigidbody>();
+            rb.isKinematic = true;
+            rb.useGravity = false;
+            
+            collider = handObject.GetComponent<Collider>();
+            collider.enabled = false;
         }
 
         if (handObject != null)
         {
-            Destroy(handObject.GetComponent<Rigidbody>());
             handObject.transform.position = stayPos.position;
         }
         
+        interactionObject = handObject;
     }
 }
