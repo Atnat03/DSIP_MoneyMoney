c394ac66a77825f4951f10475ff9804fb4a1bc5f
BOURRICAUD Antoine <a.bourricaud@rubika-edu.com>
2026-02-10 15:06:04 +0100

    fix

--- Assets/1. Scripts/Truck/DetectWall.cs
+++ Assets/1. Scripts/Truck/DetectWall.cs
@@ -5,7 +5,7 @@ public class DetectWall : MonoBehaviour
 {
     public TruckController truckController;
     
-    public void OnTriggerEnter(Collider other)
+    public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
         {
