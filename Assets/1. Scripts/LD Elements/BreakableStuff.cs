76964f90af6ea785dd68f8e139821a5a9a3b2a0b
Entity02 <114674519+Entity02@users.noreply.github.com>
2026-02-12 21:51:49 +0100

    OPTI DE FDP

--- Assets/1. Scripts/LD Elements/BreakableStuff.cs
+++ Assets/1. Scripts/LD Elements/BreakableStuff.cs
@@ -1,4 +1,6 @@
 using System;
+using Unity.Netcode;
+using Unity.Netcode.Components;
 using UnityEngine;
 
 public class BreakableStuff : MonoBehaviour
@@ -10,7 +12,13 @@ public class BreakableStuff : MonoBehaviour
         if (other.gameObject.layer == LayerMask.NameToLayer("Truck"))
         {
             if (isBandit) GetComponent<BanditBarrage>().enabled = false;
+            
             Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+            
+            gameObject.AddComponent<NetworkObject>();
+            GetComponent<NetworkObject>().Spawn();
+            gameObject.AddComponent<NetworkTransform>();
+            
             rb.AddForce(ejectionForce * other.transform.forward + new Vector3(0,ejectionForce/3,0),  ForceMode.Impulse);
             rb.AddTorque(30,0,0);
             Destroy(gameObject,10);
